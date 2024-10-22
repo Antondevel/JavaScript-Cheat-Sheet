4b85edf2e2ec88819c8a88cd92843b0c168fff85
Bezkostyi Anton <ekzupery93@gmail.com>
2024-10-23 02:35:04 +0800

    new

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function toggleSection(sectionId) {
     const content = document.getElementById(sectionId);
 
@@ -19,5 +21,43 @@ function toggleSection(sectionId) {
     }
 }
 
+// Поиск
+const searchInput = document.getElementById('search-input');
+searchInput.addEventListener('keyup', function (event) {
+    const query = event.target.value.toLowerCase();
+    const sections = document.querySelectorAll('.section');
+
+    sections.forEach(section => {
+        const items = section.querySelectorAll('li');
+        let hasMatch = false;
+
+        items.forEach(item => {
+            const text = item.textContent.toLowerCase();
+            if (text.includes(query)) {
+                item.classList.add('visible'); // Показываем подходящие элементы
+                item.style.backgroundColor = 'black'; // Выделяем найденный элемент
+                item.style.height = '100%'; // Устанавливаем высоту цветной полоски
+                item.style.paddingTop = '10px'
+                item.style.paddingBottom = '10px'
+                item.style.paddingLeft = '10px'
+                item.style.paddingRight = '10px'
+                item.style.borderRadius = '5px'; // Устанавливаем радиус скругления углов
+                hasMatch = true; // Отмечаем, что есть совпадение
+            } else {
+                item.classList.remove('visible'); // Скрываем неподходящие элементы
+                item.style.backgroundColor = ''; // Сбрасываем цвет фона
+                item.style.height = ''; // Сбрасываем высоту
+                item.style.borderRadius = ''; // Сбрасываем радиус скругления
+            }
+        });
 
 
+        // Открываем секцию, если есть совпадение
+        if (hasMatch) {
+            section.style.display = 'block';
+            toggleSection(section.querySelector('h2').getAttribute('onclick').match(/'(.+?)'/)[1]); // Открываем секцию
+        } else {
+            section.style.display = 'none'; // Скрываем секцию, если нет совпадений
+        }
+    });
+});
